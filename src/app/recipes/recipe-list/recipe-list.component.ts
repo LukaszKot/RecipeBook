a5a0df441c86726dceb99b075592f02d644d3696
Lukasz Kot <MPH743@motorolasolutions.com>
2021-01-29 15:22:11 +0100

    Added components.

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipies: Recipe[] = [
+    new Recipe('A Test Recipe', "This is a simple tests",
+      'https://i1.wp.com/www.eatthis.com/wp-content/uploads/2019/10/pumpkin-pad-thai-recipe.jpg?fit=1200%2C879&ssl=1'),
+    new Recipe('A Test Recipe', "This is a simple tests",
+      'https://i1.wp.com/www.eatthis.com/wp-content/uploads/2019/10/pumpkin-pad-thai-recipe.jpg?fit=1200%2C879&ssl=1'),
+    new Recipe('A Test Recipe', "This is a simple tests",
+      'https://i1.wp.com/www.eatthis.com/wp-content/uploads/2019/10/pumpkin-pad-thai-recipe.jpg?fit=1200%2C879&ssl=1')
+
+  ];
+  ngOnInit(): void {
+  }
+
+}
